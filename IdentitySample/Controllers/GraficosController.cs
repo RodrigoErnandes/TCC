28fdebf26e6f44b9316a8326da4f42112a117104
Rodrigo Ernandes Vicente Pinheiro <rodrigo-ernandes@hotmail.com>
2021-11-29 16:05:00 -0400

    gráficos

--- IdentitySample/Controllers/GraficosController.cs
+++ IdentitySample/Controllers/GraficosController.cs
@@ -1,9 +1,10 @@
 ﻿using IdentitySample.Models;
-using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+
 
 namespace IdentitySample.Controllers
 {
@@ -13,12 +14,57 @@ namespace IdentitySample.Controllers
 
         public ActionResult RelatorioEmprestimos()
         {
-            ViewBag.Status = new SelectList(new List<string>()
-                {
-                    {"Emprestado"},
-                    {"Devolvido"},
-                    {"Reservado"}
-              });
+            
+
+            ViewBag.ListaQtdeReservasPorMes = db.ReservaLivros.GroupBy(c => new { ano = c.Reserva.DataReserva.Year, mes = c.Reserva.DataReserva.Month })
+                    .Select(c => new GraficoViewModel
+                    {
+                        descricao = c.Key.mes + "/" + c.Key.ano,
+                        qtde = c.Count()
+                    }).ToList();
+
+            ViewBag.ListaQtdeBaixasPorMes = db.Baixas.GroupBy(c => new { ano = c.Databaixa.Year, mes = c.Databaixa.Month })
+                    .Select(c => new GraficoViewModel
+                    {
+                        descricao = c.Key.mes + "/" + c.Key.ano,
+                        qtde = c.Count()
+                    }).ToList();
+
+
+            var emprestimos = db.Emprestimos.Include(e => e.Livro);
+
+            ViewBag.ListaGeneroEmprestados = db.Acervos
+                .Include(c => c.Livro)
+                .Include(c => c.Livro.Emprestimos)
+                .GroupBy(c => new { genero = c.Genero })             
+                 .Select(c => new GraficoViewModel
+                 {
+                     descricao = c.Key.genero,
+                     qtde = c.Sum(d => d.Livro.Emprestimos.Count())
+                 }).Where(c=> c.qtde>0)
+                 .ToList();
+
+            ViewBag.ListaEstadoConservacao = db.Acervos
+                .Include(c => c.Livro)
+                .Include(c => c.Estado)
+                .GroupBy(c => new { estado = c.Estado })
+                 .Select(c => new GraficoViewModel
+                 {
+                     descricao = c.Key.estado,
+                     qtde = c.Count()
+                 }).Where(c => c.qtde > 0)
+                 .ToList();
+
+            ViewBag.ListaQtdeLivrosEmprestados = db.Emprestimos
+                .Include(c => c.Livro)           
+                .GroupBy(c => new {c.Livro.Titulo } )
+                 .Select(c => new GraficoViewModel
+                 {
+                     descricao = c.Key.Titulo,
+                     qtde = c.Count()
+                 }).Where(c => c.qtde > 0)
+                 .ToList();
+
             return View();
         }
     }
---
+++ IdentitySample/Models/GraficoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentitySample.Models
+{
+    public class GraficoViewModel
+    {
+        public string descricao { get; set; }
+        public int qtde { get; set; }
+    }
+}
